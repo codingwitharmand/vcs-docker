f0de63ccbaf348cbc58de62a552a2271b522ea3e
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2024-03-29 20:13:45 +0100

    add isUpperThan10 method

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world.");
+
+        //Check if number is upper than 10
+        int num = 5;
+        System.out.println(isUpperThan10(num));
+    }
+
+    public static boolean isUpperThan10(int  num){
+        return num > 10;
     }
 }
